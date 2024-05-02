ae1753838d4a1984f2d580de57779269fd335514
Gardeken <128183683+Gardeken@users.noreply.github.com>
2024-05-02 16:03:22 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+
+async function conectarDB() {
+    try {
+        await mongoose.connect(process.env.token)
+        console.log("Conexión a la base de datos exitosa")
+
+    } catch (error) {
+        console.log("Error al conectar a la base de datos: " + error)
+    }
+}
+conectarDB();
+
+app.use(express.json())
+app.use('/', express.static(path.resolve('views', 'home')))
+app.use('/Login', express.static(path.resolve('views', 'login')))
+app.use('/Register', express.static(path.resolve('views', 'registro')))
+
+module.exports = app
