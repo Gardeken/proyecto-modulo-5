d66b688d9e13c22abd91534e56a5d59dfa87781d
Gardeken <128183683+Gardeken@users.noreply.github.com>
2024-05-09 15:55:30 -0400

    Add files via upload

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,3 +1,14 @@
 const userRouter = require("express").Router();
+const user = require('../models/usuario')
+
+userRouter.post('/', (req, res) =>{
+    res
+    .status(404)
+    .json(
+        {
+            "message": "Hello World"
+        }
+    )
+})
 
 module.exports = userRouter;
